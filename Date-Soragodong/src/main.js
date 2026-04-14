694846ad93cb63c8785e0da0b1f7b62ca3d16e4f
ITlearning <yo7504@naver.com>
2026-04-15 00:24:55 +0900

    핵클 SDK 추가

--- Date-Soragodong/src/main.js
+++ Date-Soragodong/src/main.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { createInstance, HackleProvider } from "@hackler/react-sdk";
 
 import { App } from "./app.js";
 import { ResultPage } from "./ResultPage.js";
 import "./styles.css";
+
+const hackleClient = createInstance("BqA2Ey8jDNxzfOnJoEcQO0NE4LIIHXvP");
 
 const container = document.querySelector("#app");
 
@@ -15,4 +18,8 @@ if (!container) {
 const path = window.location.pathname;
 const Component = path.startsWith("/result") ? ResultPage : App;
 
-createRoot(container).render(React.createElement(Component));
+createRoot(container).render(
+  React.createElement(HackleProvider, { hackleClient },
+    React.createElement(Component)
+  )
+);
